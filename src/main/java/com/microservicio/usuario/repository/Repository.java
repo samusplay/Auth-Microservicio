7b0a561ccbc63bd4d9f0e29647d9841b7abbb21e
Kenneth Albadan <albadan.kenneth@gmail.com>
2026-02-25 14:31:22 -0500

    feat: Creación Repository

--- src/main/java/com/microservicio/usuario/repository/Repository.java
+++ src/main/java/com/microservicio/usuario/repository/Repository.java
@@ -3,11 +3,12 @@ package com.microservicio.usuario.repository;
 import com.microservicio.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface Repository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 
-
-
-
-
+    boolean existsByTelefono(String telefono);
 }
