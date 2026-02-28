e519677353d4c9cbd0005397d1dcffe3fa0442e9
Brandon <brayeso2018@gmail.com>
2026-02-27 19:17:14 -0500

    Finalizacion de la API de Registro

--- src/main/java/com/company/auth/api/UsuarioApi.java
+++ src/main/java/com/company/auth/api/UsuarioApi.java
@@ -3,11 +3,14 @@ package com.company.auth.api;
 import com.company.auth.models.AuthRequest;
 import com.company.auth.models.AuthResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UsuarioApi {
 
+    @PostMapping("/register")
     ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request);
 
+    @PostMapping("/login")
     ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request);
 }
