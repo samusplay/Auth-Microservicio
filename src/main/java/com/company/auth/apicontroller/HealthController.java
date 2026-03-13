67d2138abba7b3eface72f0b4c868b43854d4209
Xertz <stiven131bee@gmail.com>
2026-03-13 00:21:22 -0500

    feat: implement health check endpoint

---
+++ src/main/java/com/company/auth/apicontroller/HealthController.java
@@ -0,0 +1,14 @@
+package com.company.auth.apicontroller;
+
+import com.company.auth.api.HealthApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController implements HealthApi {
+
+    @Override
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Servicio de Autenticación funcionando correctamente");
+    }
+}
